2dac1d1de69621536e7a921a30f20792d25d8bfb
Neema <patelneema93@gmail.com>
2024-04-30 10:12:19 -0400

    props pass h3

--- redux/src/component/User.jsx
+++ redux/src/component/User.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 const User = (props) =>{
-    console.warn(props)
+    console.warn(props.data.name)
+    const {data} = props
     return(
         <div>
             <h1> User component</h1>
+            <h3>{data.name}</h3>
         </div>
     )
 }
