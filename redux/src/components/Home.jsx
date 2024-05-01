a23f849daeb0394edeea60679a884f595e571611
Neema <patelneema93@gmail.com>
2024-05-01 09:53:11 -0400

    add css in file

--- redux/src/components/Home.jsx
+++ redux/src/components/Home.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import './HomeEdit.css';
 function Home() {
     return(
         <div>
             <h1>Home Component</h1>
             <div className="cart-wrapper">
-                <div className="text-wrapper item">
+                <div className="img-wrapper item">
                     <img src="https://cdn.alloallo.media/catalog/product/apple/iphone/iphone-13/iphone-13-pink.jpg" />
 
                 </div>
                 <div className="text-wrapper item">
-                   <span>I-Phone</span>
+                   <span>I-Phone</span><br/>
                    <span>Price: 1000$</span>
 
                 </div>
-                <div className="text-wrapper item">
+                <div className="btn-wrapper item">
                     <button>Add to cart</button>
 
                 </div>
