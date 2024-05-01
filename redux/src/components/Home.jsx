6f6ecb7434934937e51518ed265590b50c8af1af
Neema <patelneema93@gmail.com>
2024-05-01 09:06:43 -0400

    create cart stucture in home

--- redux/src/components/Home.jsx
+++ redux/src/components/Home.jsx
@@ -3,6 +3,22 @@ function Home() {
     return(
         <div>
             <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="text-wrapper item">
+                    <img src="https://cdn.alloallo.media/catalog/product/apple/iphone/iphone-13/iphone-13-pink.jpg" />
+
+                </div>
+                <div className="text-wrapper item">
+                   <span>I-Phone</span>
+                   <span>Price: 1000$</span>
+
+                </div>
+                <div className="text-wrapper item">
+                    <button>Add to cart</button>
+
+                </div>
+
+            </div>
         </div>
     )
 }
