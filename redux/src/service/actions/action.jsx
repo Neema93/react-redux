aba529dd21cf82935ada16633705bc865dc194d0
Neema <patelneema93@gmail.com>
2024-05-07 10:09:06 -0400

    create reducer file

--- redux/src/service/actions/action.jsx
+++ redux/src/service/actions/action.jsx
@@ -1,6 +1,7 @@
-const addToCart= (data) =>{
+import { ADD_TO_CART } from "../constants"
+export const addToCart= (data) =>{
     return{
-        type:'ADD_TO_CART',
+        type:ADD_TO_CART,
         data: data
     }
 }
