73ffca3042e9db59f6132cc0166b6e0ce4bbfbfc
Neema <patelneema93@gmail.com>
2024-05-10 09:37:36 -0400

    display card item

--- redux/src/containers/HomeContainers.jsx
+++ redux/src/containers/HomeContainers.jsx
@@ -3,7 +3,7 @@ import Home from "../components/Home"
 import {connect} from 'react-redux'
 import { addToCart } from "../service/actions/action";
 const mapStateToProps=state=>({
-    data:state
+    data:state.cardItems
 })
 const mapDispatchProps=dispatch=>({
     addToCartHandler:data=>dispatch(addToCart(data))
