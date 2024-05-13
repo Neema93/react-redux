8690db3c049c51ce467161844470e060359dea58
Neema <patelneema93@gmail.com>
2024-05-13 10:36:34 -0400

    remove mapstate in home container

--- redux/src/containers/HomeContainers.jsx
+++ redux/src/containers/HomeContainers.jsx
@@ -2,10 +2,8 @@
 import Home from "../components/Home"
 import {connect} from 'react-redux'
 import { addToCart } from "../service/actions/action";
-const mapStateToProps=state=>({
-    // data:state.cardItems
-})
+
 const mapDispatchProps=dispatch=>({
     addToCartHandler:data=>dispatch(addToCart(data))
 })
-export default connect(mapStateToProps,mapDispatchProps)(Home);
+export default connect(mapDispatchProps)(Home);
