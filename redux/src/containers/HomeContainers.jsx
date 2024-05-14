7c3cb3bfcd455b9e954ebe432cad21103ea2c96c
Neema <patelneema93@gmail.com>
2024-05-14 10:11:28 -0400

    HomeContainers add removeToCart

--- redux/src/containers/HomeContainers.jsx
+++ redux/src/containers/HomeContainers.jsx
@@ -1,11 +1,12 @@
 
 import Home from "../components/Home"
 import {connect} from 'react-redux'
-import { addToCart } from "../service/actions/action";
+import { addToCart, removeToCart } from "../service/actions/action";
 const mapStateToProps=state=>({
     // data:state.cardItems
 })
 const mapDispatchProps=dispatch=>({
-    addToCartHandler:data=>dispatch(addToCart(data))
+    addToCartHandler:data=>dispatch(addToCart(data)),
+    removeToCartHandler:()=>dispatch(removeToCart())
 })
 export default connect(mapStateToProps,mapDispatchProps)(Home);
