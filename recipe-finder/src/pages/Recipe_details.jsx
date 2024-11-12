ab4096d312923a9771681eacae46290262336ed0
Neema <patelneema93@gmail.com>
2024-11-12 13:49:27 -0500

    add basic details in all page

---
+++ recipe-finder/src/pages/Recipe_details.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Recipe_details = () => {
+  return (
+    <div>
+      <h1>Recipe_details Page</h1>
+      
+    </div>
+  );
+};
+
+export default Recipe_details;
---
+++ recipe-finder/src/pages/Recipe_search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Recipe_search = () => {
+  return (
+    <div>
+      <h1>Recipe_search Page</h1>
+      
+    </div>
+  );
+};
+
+export default Recipe_search;
