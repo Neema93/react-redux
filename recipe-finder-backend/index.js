4d3c9efb32fc211434af91f9c575c6e2ab513a3f
Neema <patelneema93@gmail.com>
2024-11-15 13:08:14 -0500

    create a index.js file import

---
+++ recipe-finder-backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
