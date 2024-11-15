b210d97bddb1405356e13529ba961ce35d8db4a4
Neema <patelneema93@gmail.com>
2024-11-15 13:14:51 -0500

    create pool for canet for database

--- recipe-finder-backend/index.js
+++ recipe-finder-backend/index.js
@@ -10,3 +10,13 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// Create a new PostgreSQL client using the environment variables
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+
+})
