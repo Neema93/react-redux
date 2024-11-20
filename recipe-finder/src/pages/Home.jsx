703ef96f83579dc8a951315de285576d9794e213
Neema <patelneema93@gmail.com>
2024-11-20 15:55:22 -0500

    create recipe table

--- recipe-finder/src/pages/Home.jsx
+++ recipe-finder/src/pages/Home.jsx
@@ -4,11 +4,36 @@ import axios from 'axios';
 import Recipe from './components/Recipe';
 
 const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/recipes')
+      .then(response => {
+        setRecipes(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the recipes!', error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading recipes...</div>;
+  }
+
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Home Page!</p>
-    </div>
+    <div >
+    <h1>Recipe List</h1>
+    {recipes.length === 0 ? (
+      <p>No recipes found.</p>
+    ) : (
+      recipes.map(recipe => (
+        <Recipe key={recipe.id} recipe={recipe} />
+      ))
+    )}
+  </div>
   );
 };
 
